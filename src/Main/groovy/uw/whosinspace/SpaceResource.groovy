c858b0e32524c58d06ebe5945ff3b4fe5af7ff49
AlanBrown <dantebot@gmail.com>
2016-09-13 14:21:26 +0100

    added ISS Position viewer

--- src/Main/groovy/uw/whosinspace/SpaceResource.groovy
+++ src/Main/groovy/uw/whosinspace/SpaceResource.groovy
@@ -1,5 +1,6 @@
 package uw.whosinspace
 
+import groovy.json.JsonSlurper
 import jodd.http.HttpRequest
 import jodd.http.HttpResponse
 
@@ -16,9 +17,16 @@ class SpaceResource {
     @GET
     public def whosInSpace()
     {
+        def slurper = new JsonSlurper()
         HttpRequest httpRequest = HttpRequest.get('http://api.open-notify.org/astros.json')
         HttpResponse response = httpRequest.send()
-        response.body()
+        def people = slurper.parseText(response.body())
+        httpRequest = HttpRequest.get('http://api.open-notify.org/iss-now.json')
+        response = httpRequest.send()
+        def position = slurper.parseText(response.body())
+        String mapurl = "https://maps.google.com/maps?q=${position.iss_position.latitude},${position.iss_position.longitude}&hl=es;z=14&amp;&output=embed&z=2"
+        [roster:people, position:position, mapurl:mapurl]
+
     }
 
 }
